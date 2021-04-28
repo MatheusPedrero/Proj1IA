3ac7cacb15bf74eea1c284214a447a1bc8c1f0a9
MatheusPedrero <matheus.pedrero@edu.unifil.br>
2021-04-28 20:11:00 -0300

    Primeiros ajustes

--- esqueleto-jogo-agentes-ia-master/regras_jogo/regras_abstratas.py
+++ esqueleto-jogo-agentes-ia-master/regras_jogo/regras_abstratas.py
@@ -1,47 +1,75 @@
 from abc import ABC, abstractmethod
 
 class AbstractRegrasJogo(ABC):
-    """ Interface mínima para implementar um jogo interativo e modular. Não
-    tente instanciar objetos dessa classe, ela deve ser herdada e seus métodos
-    abstratos sobrecarregados.
-    """
+    def init(self) -> None:
+        super().init()
+        mapa_labirinto = { 
+                [ ['+','+','+','+',...,'+','+','+','+','+','+','+'],
+                  ['+',' ',' ',' ',...,' ',' ',' ','+',' ',' ',' '],
+                  ['+',' ','+',' ',...,'+','+',' ','+',' ','+','+'],
+                  ['+',' ','+',' ',...,' ',' ',' ','+',' ','+','+'],
+                  ['+','+','+',' ',...,'+','+',' ','+',' ',' ','+'],
+                  ['+',' ',' ',' ',...,'+','+',' ',' ',' ',' ','+'],
+                  ['+','+','+','+',...,'+','+','+','+','+',' ','+'],
+                  ['+',' ',' ',' ',...,'+','+',' ',' ','+',' ','+'],
+                  ['+',' ','+','+',...,' ',' ','+',' ',' ',' ','+'],
+                  ['+',' ',' ',' ',...,' ',' ','+',' ','+','+','+'],
+                  ['+','+','+','+',...,'+','+','+',' ','+','+','+']]
+            }
+
+        self.labirinto = mapa_labirinto
+        self.id_personagens = {Personagens.JOGADOR: 0}
+        self.acoes_personagens = {0: None}
+        self.msg_jogador = None
 
     @abstractmethod
     def registrarAgentePersonagem(self, personagem):
-        """ Cria ou recupera id de um personagem agente.
-        """
-        return
+        return self.id_personagens[personagem]
     
     @abstractmethod
-    def isFim(self):
-        """ Boolean indicando fim de jogo em True.
-        """
-        return
+    def isFim(self,linha,coluna):
+        return(linha == 0 or
+             linha == self.linhas-1 or
+             coluna == 0 or
+             coluna == self.parede_adjacente-1 )
 
     @abstractmethod
     def gerarCampoVisao(self, id_agente):
-        """ Retorna um EstadoJogoView para ser consumido por um agente
-        específico. Objeto deve conter apenas descrição de elementos visíveis
-        para este agente.
+        percepcoes_jogador = PercepcoesJogador(
+            pos_jogador=set(self.labirinto),
+            dimensoes=(11, 11),
+            mensagem_jogo=self.msg_jogador)
 
-        EstadoJogoView é um objeto imutável ou uma cópia do jogo, de forma que
-        sua manipulação direta não tem nenhum efeito no mundo de jogo real.
-        """
-        return
+        self.msg_jogador = None
+        return percepcoes_jogador
 
     @abstractmethod
     def registrarProximaAcao(self, id_agente, acao):
-        """ Informa ao jogo qual a ação de um jogador especificamente.
-        Neste momento, o jogo ainda não é transformado em seu próximo estado,
-        isso é feito no método de atualização do mundo.
-        """
+
+        self.acoes_personagens[id_agente] = acao
+
         return
     
     @abstractmethod
     def atualizarEstado(self, diferencial_tempo):
-        """ Apenas neste momento o jogo é atualizado para seu próximo estado
-        de acordo com as ações de cada jogador registradas anteriormente.
-        """
+        acao_jogador = self.acoes_personagens[
+            self.id_personagem[Personagens.JOGADOR]]
+        if acao_jogador.tipo == AcoesJogador.MOVER_BOLINHA:
+            x, y, direcao = acao_jogador.parametros
+
+            if (x, y) in self.labirinto:
+                x_mov, y_mov = self.decodificar_direcao(direcao)
+
+                parede_adjacente = (x + x_mov, y + y_mov)
+                espaco_pos_adjacente = (x + x_mov2, y + y_mov2)
+                if self.is_jogada_valida(parede_adjacente, espaco_pos_adjacente):
+                    self.labirinto.discard(parede_adjacente)
+                    self.labirinto.discard((x,y))
+                    self.labirinto.add(espaco_pos_adjacente)
+                    else:
+                        self.msg_jogador = f'Impossivel de realizar o movimento, parede a frente.'
+            else:
+                self.msg_jogador = f'Tecla inválida.'
         return
     
     @abstractmethod
